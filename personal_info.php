c9e9aa06704393e909a43cbb55bd609de0c0a5d7
Md. Ashikuzzaman <wpashik94@gmail.com>
2023-09-19 02:33:14 +0600

    Update personal_info.php

--- personal_info.php
+++ personal_info.php
@@ -1,3 +1,24 @@
-# assignment-of-module-1
+<!DOCTYPE html>
+<html>
 
-hi my name is ashikuzzaman
+<head>
+    <title>Grade Calculator</title>
+</head>
+
+<body>
+    <?php
+        $name = "Md. Ashikuzzaman";
+        $age = 30; // Replace with your actual age
+        $country = "Bangladesh";
+        $introduction = "Hello! My name is $name. I am $age years old and I am from $country.";
+
+        // Display personal information
+        echo "<h1>Personal Information</h1>";
+        echo "<p><strong>Name:</strong> $name</p>";
+        echo "<p><strong>Age:</strong> $age</p>";
+        echo "<p><strong>Country:</strong> $country</p>";
+        echo "<p><strong>Introduction:</strong> $introduction</p>";
+    ?>
+</body>
+
+</html>
